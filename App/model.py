c41c20bdb7db78368323e489e9a8b11af67baf15
David Fuquen <d.fuquen@uniandes.edu.co>
2021-12-01 21:38:57 -0500

    Haversine

--- App/model.py
+++ App/model.py
@@ -33,8 +33,8 @@ from DISClib.Algorithms.Sorting import shellsort as sa
 from DISClib.ADT.graph import gr
 from DISClib.Utils import error as error
 assert cf
-from DISClib.ADT import orderedmap as om
-
+from DISClib.ADT import orderedmap as om        
+from math import radians, cos, sin, asin, sqrt
 """
 Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
 los mismos.
@@ -265,7 +265,23 @@ def getInfo(analyzer, id):
 
 
 
+#Función para calcular la distancia en km de dos puntos con longitud y latitud. Retribuido del link disponible en la guía del laboratorio
+#https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points creada por Michale Dunn para Stack Overflow
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 
 
